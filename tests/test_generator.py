038d464e95256274224bc508cbb49874ae879074
xiongkun <xiongkun03@baidu.com>
2023-04-18 06:22:00 +0000

    add unittest for generator.

---
+++ tests/test_generator.py
@@ -0,0 +1,21 @@
+import paddle
+from symbolic_trace import symbolic_trace
+import unittest
+from test_case_base import TestCaseBase
+
+def gen():
+    for i in range(10):
+        yield i
+
+def case1():
+    sum = 0
+    for i in gen():
+        sum += i 
+    return sum
+
+class TestGen(TestCaseBase):
+    def test_gen(self):
+        self.assert_results(case1)
+
+if __name__ == "__main__":
+    unittest.main()
