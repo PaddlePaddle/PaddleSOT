6d4c3727021fcd365ef5e01dcbf0e41869db79eb
wuzhanfei <wuzhanfei@baidu.com>
2023-04-26 07:39:10 +0000

    add test

---
+++ tests/test_executor/test_03_tuple.py
@@ -0,0 +1,25 @@
+import unittest
+
+from test_case_base import TestCaseBase
+
+import paddle
+
+
+def foo(x: int, y: paddle.Tensor):
+    x = (x, y)
+    return x[1] + 1
+
+
+class TestExecutor(TestCaseBase):
+    def test_simple(self):
+        self.assert_results(foo, 1, paddle.to_tensor(2))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+# Instructions:
+# BUILD_TUPLE (new)
+# BINARY_SUBSCR (new)
+# LOAD_CONST (new)
