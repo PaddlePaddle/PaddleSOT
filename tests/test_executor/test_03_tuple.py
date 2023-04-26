d7d4412beb64fea511846dcc6c4e6ae8f40d6e70
feifei-111 <2364819892@qq.com>
2023-04-26 18:56:55 +0800

    Build List/tuple (#35)
    * build list / tuple
    
    * ignore E722 check
    
    * add trace_value_from_frame and set source when __getitem__

---
+++ tests/test_executor/test_03_tuple.py
@@ -0,0 +1,24 @@
+# New Supported Instructions:
+# BUILD_TUPLE
+# BINARY_SUBSCR
+
+
+import unittest
+
+from test_case_base import TestCaseBase
+
+import paddle
+
+
+def foo(x: int, y: paddle.Tensor):
+    x = (x, y)
+    return x[1] + 1
+
+
+class TestExecutor(TestCaseBase):
+    def test_simple(self):
+        self.assert_results(foo, 1, paddle.to_tensor(2))
+
+
+if __name__ == "__main__":
+    unittest.main()
