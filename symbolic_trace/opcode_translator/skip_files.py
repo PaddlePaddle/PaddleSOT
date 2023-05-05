2fb12a768559e46e1a89240b78e6ba81ac32ce69
xiongkun <xiongkun03@baidu.com>
2023-05-05 11:03:06 +0800

    add uuid to skip_files (#38)

--- symbolic_trace/opcode_translator/skip_files.py
+++ symbolic_trace/opcode_translator/skip_files.py
@@ -27,6 +27,7 @@ import traceback
 import types
 import typing
 import unittest
+import uuid
 import weakref
 
 import _collections_abc
@@ -79,6 +80,7 @@ skip_file_names = {
         _weakrefset,
         decorator,
         codecs,
+        uuid,
     )
 }
 
