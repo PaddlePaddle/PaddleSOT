109bf7dbfdd72fda8d0dadcc8fb1cbb84c9ad388
xiongkun <xiongkun03@baidu.com>
2023-05-09 07:17:31 +0000

    add some testcase for paddle-api, tensor-method...

--- tests/test_executor/test_06_call_function.py
+++ tests/test_executor/test_06_call_function.py
@@ -25,10 +25,35 @@ def foo_2(x: paddle.Tensor):
     return y
 
 
+def foo_3(x: paddle.Tensor):
+    m = x + 1
+    y = sub(m * 3, m * 2)
+    y = sub(y, y)
+    y = sub(y, y)
+    return y
+
+
+def nest_2(x):
+    return x + 1
+
+
+def nest_1(x):
+    return (x - 1) * 2
+
+
+def foo_4(x: paddle.Tensor):
+    m = x + 1
+    m = nest_1(m)
+    return m
+
+
 class TestExecutor(TestCaseBase):
     def test_simple(self):
         self.assert_results(foo_1, paddle.to_tensor(2))
         self.assert_results(foo_2, paddle.to_tensor(2))
+        self.assert_results(foo_3, paddle.to_tensor(2))
+        # TODO: FunctionConstTracker is missing.
+        # self.assert_results(foo_4, paddle.to_tensor(2))
 
 
 if __name__ == "__main__":
