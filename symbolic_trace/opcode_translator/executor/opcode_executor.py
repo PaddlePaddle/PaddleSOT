f4cccbf688ff7d9e54e733e2914c2b7ac2e13500
feifei-111 <2364819892@qq.com>
2023-05-10 02:56:16 +0000

    update

--- symbolic_trace/opcode_translator/executor/opcode_executor.py
+++ symbolic_trace/opcode_translator/executor/opcode_executor.py
@@ -36,7 +36,6 @@ from .tracker import (
     GetItemTracker,
     GlobalTracker,
     LocalTracker,
-    VariableTracker,
 )
 from .variables import (
     ConstantVariable,
@@ -46,6 +45,7 @@ from .variables import (
     ListVariable,
     TensorVariable,
     TupleVariable,
+    VariableTracker,
     VariableTrackerFactory,
 )
 
