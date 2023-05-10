28eb173495ed88b01d21299305883eac038df823
SigureMo <sigure.qaq@gmail.com>
2023-05-10 19:24:07 +0800

    add more uts

--- tests/test_executor/test_output_restore.py
+++ tests/test_executor/test_output_restore.py
@@ -7,14 +7,22 @@ from test_case_base import TestCaseBase
 import paddle
 
 
-def foo(x: paddle.Tensor, y: paddle.Tensor, z: int):
+def output_identity(x):
+    return x
+
+
+def output_const():
+    return 42
+
+
+def output_list(x: paddle.Tensor, y: paddle.Tensor, z: int):
     a = x + 1
     b = z + 1
     l = [1, a, b, y]
     return l
 
 
-def bar(x: paddle.Tensor, y: paddle.Tensor, z: int):
+def output_nest_struct(x: paddle.Tensor, y: paddle.Tensor, z: int):
     a = x + y + z
     b = z + 1
     l = [1 + 1, (z, a), [b]]
@@ -22,17 +30,25 @@ def bar(x: paddle.Tensor, y: paddle.Tensor, z: int):
 
 
 class TestOutputRestore(TestCaseBase):
-    def test_foo(self):
+    def test_output_identity(self):
+        self.assert_results(output_identity, 1)
+        self.assert_results(output_identity, 2)
+        self.assert_results(output_identity, paddle.to_tensor(1))
+
+    def test_output_const(self):
+        self.assert_results(output_const)
+
+    def test_output_list(self):
         a = paddle.to_tensor(1)
         b = paddle.to_tensor(2)
 
-        self.assert_results(foo, a, b, 3)
+        self.assert_results(output_list, a, b, 3)
 
-    def test_bar(self):
+    def test_output_nest_struct(self):
         a = paddle.to_tensor(1)
         b = paddle.to_tensor(2)
 
-        self.assert_results(bar, a, b, 3)
+        self.assert_results(output_nest_struct, a, b, 3)
 
 
 if __name__ == "__main__":
