6c25833bbb6271cf51335290f6742969827958db
SigureMo <sigure.qaq@gmail.com>
2023-05-11 10:19:37 +0800

    add set support to assert_nest_match

--- tests/test_executor/test_case_base.py
+++ tests/test_executor/test_case_base.py
@@ -13,23 +13,23 @@ class TestCaseBase(unittest.TestCase):
         self.assertIs(
             cls_x, cls_y, msg=f"type mismatch, x is {cls_x}, y is {cls_y}"
         )
-        if cls_x in (tuple, list):
+        container_types = (tuple, list, dict, set)
+        if cls_x in container_types:
             self.assertEqual(
                 len(x),
                 len(y),
                 msg=f"length mismatch, x is {len(x)}, y is {len(y)}",
             )
-            for x_item, y_item in zip(x, y):
-                self.assert_nest_match(x_item, y_item)
-        elif cls_x is dict:
-            self.assertEqual(
-                len(x),
-                len(y),
-                msg=f"length mismatch, x is {len(x)}, y is {len(y)}",
-            )
-            for x_key, y_key in zip(x.keys(), y.keys()):
-                self.assert_nest_match(x_key, y_key)
-                self.assert_nest_match(x[x_key], y[y_key])
+            if cls_x in (tuple, list):
+                for x_item, y_item in zip(x, y):
+                    self.assert_nest_match(x_item, y_item)
+            elif cls_x is dict:
+                for x_key, y_key in zip(x.keys(), y.keys()):
+                    self.assert_nest_match(x_key, y_key)
+                    self.assert_nest_match(x[x_key], y[y_key])
+            elif cls_x is set:
+                # TODO: Nested set is not supported yet
+                self.assertEqual(x, y)
         elif cls_x in (np.ndarray, paddle.Tensor):
             np.testing.assert_allclose(x, y)
         else:
