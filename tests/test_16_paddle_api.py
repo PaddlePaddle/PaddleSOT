f805b0f6ae6ff176e5eac4cf09970c522439c83c
SigureMo <sigure.qaq@gmail.com>
2023-05-26 17:00:42 +0800

    remove comment flag

--- tests/test_16_paddle_api.py
+++ tests/test_16_paddle_api.py
@@ -25,15 +25,15 @@ def paddle_api_function_call_concat(
 
 
 class TestPaddleApiCall(TestCaseBase):
-    # def test_paddle_api_method_call(self):
-    #     self.assert_results(paddle_api_method_call, paddle.to_tensor(2.0))
-    #     self.assert_results(paddle_api_method_call, paddle.to_tensor(-5.0))
-    #     self.assert_results(paddle_api_method_call, paddle.to_tensor(0.0))
+    def test_paddle_api_method_call(self):
+        self.assert_results(paddle_api_method_call, paddle.to_tensor(2.0))
+        self.assert_results(paddle_api_method_call, paddle.to_tensor(-5.0))
+        self.assert_results(paddle_api_method_call, paddle.to_tensor(0.0))
 
-    # def test_paddle_api_function_call(self):
-    #     self.assert_results(paddle_api_function_call, paddle.to_tensor(2.0))
-    #     self.assert_results(paddle_api_function_call, paddle.to_tensor(-5.0))
-    #     self.assert_results(paddle_api_function_call, paddle.to_tensor(0.0))
+    def test_paddle_api_function_call(self):
+        self.assert_results(paddle_api_function_call, paddle.to_tensor(2.0))
+        self.assert_results(paddle_api_function_call, paddle.to_tensor(-5.0))
+        self.assert_results(paddle_api_function_call, paddle.to_tensor(0.0))
 
     def test_paddle_api_function_call_concat(self):
         a = paddle.to_tensor([[1, 2], [3, 4]])
