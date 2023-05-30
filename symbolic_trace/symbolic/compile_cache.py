cc8359b3c0c0c377aa68afebef010681b037aacf
xiongkun <xiongkun03@baidu.com>
2023-05-30 02:40:34 +0000

    add fallback wrapper for speed u p

--- symbolic_trace/symbolic/compile_cache.py
+++ symbolic_trace/symbolic/compile_cache.py
@@ -4,6 +4,25 @@ from ..utils import Cache, Singleton
 from .interpreter import compile_sir
 
 
+class FallbackWrapper:
+    def __init__(self, compile_sir):
+        self.compile_sir = compile_sir
+        self.partial_program_layer = None
+
+    def __call__(self, *args, **kwargs):
+        frame_callback = paddle.fluid.core.set_eval_frame(None)
+        if self.partial_program_layer is None:
+            outputs = self.compile_sir(*args, **kwargs)
+            self.partial_program_layer = self.compile_sir.get_concrete_program(
+                *args, **kwargs
+            )[1]
+        else:
+            # Speed up Resnet from 0.0068 --> 0.0057
+            outputs = self.partial_program_layer(*args, **kwargs)
+        paddle.fluid.core.set_eval_frame(frame_callback)
+        return outputs
+
+
 @Singleton
 class CompileSIRCache(Cache):
     def __init__(self):
@@ -15,4 +34,6 @@ class CompileSIRCache(Cache):
         return hash_key
 
     def value_fn(self, context, sir_name):
-        return paddle.jit.to_static(compile_sir(context, sir_name))
+        return FallbackWrapper(
+            paddle.jit.to_static(compile_sir(context, sir_name))
+        )
