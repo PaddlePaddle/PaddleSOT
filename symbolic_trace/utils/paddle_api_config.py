09a9060fd6ffed750a99dd1c20f4a9cb86328fed
SigureMo <sigure.qaq@gmail.com>
2023-06-01 20:22:32 +0800

    exclude non-operator-related API

--- symbolic_trace/utils/paddle_api_config.py
+++ symbolic_trace/utils/paddle_api_config.py
@@ -19,13 +19,30 @@ def get_paddle_api():
         paddle.signal,
         paddle.fft,
     ]
+    non_operator_related_apis = [
+        paddle.in_dynamic_mode,
+        paddle.save,
+        paddle.load,
+        paddle.get_cuda_rng_state,
+        paddle.set_rng_state,
+        paddle.set_cuda_rng_state,
+        paddle.get_rng_state,
+        paddle.set_default_dtype,
+        paddle.check_shape,
+        paddle.summary,
+        paddle.finfo,
+        paddle.iinfo,
+        paddle.enable_static,
+        paddle.disable_static,
+        paddle.is_grad_enabled,
+    ]
     paddle_api_list = []
     for module in modules:
         for fn_name in getattr(module, "__all__", []):
             fn = getattr(module, fn_name)
             if inspect.isfunction(fn):
                 paddle_api_list.append(fn)
-    return list(set(paddle_api_list))
+    return list(set(paddle_api_list) - set(non_operator_related_apis))
 
 
 paddle_tensor_methods = get_tensor_methods()
