7cbe81442ce6e443890fa9b1d2288ceb45ae6c91
Zhan Rongrui <2742392377@qq.com>
2023-06-02 03:36:01 +0000

    fix import

--- symbolic_trace/opcode_translator/executor/variables/__init__.py
+++ symbolic_trace/opcode_translator/executor/variables/__init__.py
@@ -1,6 +1,42 @@
-from .base import *  # noqa F401
-from .callable import *  # noqa F401
-from .container import *  # noqa F401
-from .iter import *  # noqa F401
-
-# TODO(zrr1999): import determinant
+from .base import (  # noqa F401
+    ConstantVariable,
+    ConstTypes,
+    DygraphTracerVariable,
+    ModuleVariable,
+    ObjectVariable,
+    SliceVariable,
+    TensorVariable,
+    VariableBase,
+    VariableFactory,
+    get_zero_degree_vars,
+    map_variables,
+    topo_sort_vars,
+)
+from .callable import (  # noqa F401
+    BuiltinVariable,
+    CallableVariable,
+    DirectlyCallMethodVariable,
+    FunctionVariable,
+    LayerVariable,
+    MethodVariable,
+    PaddleApiVariable,
+    PaddleLayerVariable,
+    TensorMethodVariable,
+    UserDefinedFunctionVariable,
+    UserDefinedGeneratorVariable,
+    UserDefinedLayerVariable,
+    UserDefinedMethodVariable,
+)
+from .container import (  # noqa F401
+    ContainerVariable,
+    DictVariable,
+    ListVariable,
+    TupleVariable,
+)
+from .iter import (  # noqa F401
+    DictIterVariable,
+    IterVariable,
+    SequenceIterVariable,
+    TensorIterVariable,
+    UserDefinedIterVariable,
+)
