f91a379df88ff6100a815b8fef06b8bcad5dd11e
Zhan Rongrui <2742392377@qq.com>
2023-06-09 09:25:52 +0000

    add tests

---
+++ tests/test_exception.py
@@ -0,0 +1,43 @@
+import unittest
+
+import paddle
+from symbolic_trace import symbolic_trace
+
+
+def case1(x):
+    return n  # noqa: F821
+
+
+def case2(x):
+    x = x + 1
+    return x @ x
+
+
+def case3(x):
+    y = x.undefined_attr
+    return y
+
+
+def case4(x):
+    y = x * 2
+    print(x)
+    y = y + 1
+    return y[100]
+
+
+class TestAnalysisInputs(unittest.TestCase):
+    def catch_error(self, func, *inputs):
+        try:
+            symbolic_trace(func)(*inputs)
+        except Exception as e:
+            print(e)
+
+    def test_all_case(self):
+        self.catch_error(case1, paddle.rand([2, 1]))
+        self.catch_error(case2, paddle.rand([2, 1]))
+        self.catch_error(case3, paddle.rand([2, 1]))
+        self.catch_error(case4, paddle.rand([2, 1]))
+
+
+if __name__ == "__main__":
+    unittest.main()
