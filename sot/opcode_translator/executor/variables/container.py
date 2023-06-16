fdbfb1a3129e9dbddbb3314b76bae30f8c7799af
feifei-111 <2364819892@qq.com>
2023-06-16 16:45:29 +0800

    fix name generator & support list.__mul__ (#172)

--- sot/opcode_translator/executor/variables/container.py
+++ sot/opcode_translator/executor/variables/container.py
@@ -142,6 +142,21 @@ class ListVariable(ContainerVariable):
         )
         return new_list_variable
 
+    def repeat(self, length):
+        assert isinstance(length, ConstantVariable)
+        new_list_variable = ListVariable(
+            self.get_wrapped_items() * length.value,
+            self.graph,
+            DummyTracker([self, length]),
+        )
+        return new_list_variable
+
+    def __add__(self, list_):
+        return self.concat(list_)
+
+    def __mul__(self, length):
+        return self.repeat(length)
+
     @VariableFactory.register_from_value()
     def from_value(value: Any, graph: FunctionGraph | None, tracker: Tracker):
         if isinstance(value, list):
