c91cd0353c5ebcc6a494309a068ad03b71640b15
SigureMo <sigure.qaq@gmail.com>
2023-06-18 03:34:31 +0800

    Revert "test on ci"
    This reverts commit 7fe90da40f4c0f3d0427bb21867d3b0dcaa51a41.

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -7,7 +7,7 @@ failed_tests=()
 for file in ./test_*.py; do
     # 检查文件是否为 Python 文件
     if [ -f "$file" ]; then
-        echo Running: PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 LOG_LEVEL=3 python " $file
+        echo Running: PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
         # 执行文件
         python "$file"
         if [ $? -ne 0 ]; then
