8689ec55ad34e556664c4cfcce1c9cb909022849
SigureMo <sigure.qaq@gmail.com>
2023-06-18 14:55:44 +0800

    remove useless cache

--- sot/opcode_translator/instruction_utils/opcode_analysis.py
+++ sot/opcode_translator/instruction_utils/opcode_analysis.py
@@ -6,20 +6,11 @@ from .instruction_utils import Instruction
 from .opcode_info import ALL_JUMP, HAS_FREE, HAS_LOCAL
 
 
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass
 class State:
     reads: set[str]
     writes: set[str]
     visited: set[int]
-
-    def __hash__(self):
-        return hash(
-            (
-                frozenset(self.reads),
-                frozenset(self.writes),
-                frozenset(self.visited),
-            )
-        )
 
 
 def analysis_inputs(
@@ -28,30 +19,21 @@ def analysis_inputs(
     stop_instr_idx: int | None = None,
 ):
     root_state = State(set(), set(), set())
-    branches: dict[tuple[int, bool, State], set[str]] = {
-        # (current_instr_idx, True, root_state): root_state
-    }
 
     def fork(
         state: State, start: int, jump: bool, jump_target: int
     ) -> set[str]:
-        # The cache key contains: start of jump, whether jump or not, original state
-        cache_key = (start, jump, state)
         new_start = start + 1 if not jump else jump_target
         new_state = State(
             set(state.reads), set(state.writes), set(state.visited)
         )
-        if cache_key not in branches:
-            reads = walk(new_state, new_start)
-            branches[cache_key] = reads
-            return reads
-        return branches[cache_key]
+        return walk(new_state, new_start)
 
     def walk(state: State, start: int) -> set[str]:
         end = len(instructions) if stop_instr_idx is None else stop_instr_idx
         for i in range(start, end):
             if i in state.visited:
-                continue
+                return state.reads
             state.visited.add(i)
 
             instr = instructions[i]
@@ -71,6 +53,6 @@ def analysis_inputs(
                 return not_jump_branch | jump_branch
             elif instr.opname == "RETURN_VALUE":
                 return state.reads
-        return set()
+        return state.reads
 
     return walk(root_state, current_instr_idx)
