a22ea6e425987c17950236805dddc08d0bd33a2c
Nyakku Shigure <sigure.qaq@gmail.com>
2023-06-22 18:19:02 +0800

    use dispatch instead of monkey patch (#182)

--- sot/opcode_translator/executor/__init__.py
+++ sot/opcode_translator/executor/__init__.py
@@ -1,1 +1,1 @@
-from . import variable_monkey_patch  # noqa F401
+from . import variable_dispatch  # noqa: F401
--- tests/test_sir_rollback.py
+++ tests/test_sir_rollback.py
@@ -1,23 +1,27 @@
 from __future__ import annotations
 
 import inspect
+import operator
 import unittest
 
 from test_case_base import TestCaseBase
 
 import paddle
 from sot.opcode_translator.executor.function_graph import FunctionGraph
-from sot.opcode_translator.executor.tracker import DummyTracker
-from sot.opcode_translator.executor.variables import VariableFactory
+from sot.opcode_translator.executor.tracker import DanglingTracker, LocalTracker
+from sot.opcode_translator.executor.variables import (
+    BuiltinVariable,
+    VariableFactory,
+)
 
 
 def compute(x, y):
-    ret = x + y
-    return ret * x
+    ret = BuiltinVariable(operator.add, x.graph, DanglingTracker())(x, y)
+    return BuiltinVariable(operator.mul, x.graph, DanglingTracker())(ret, x)
 
 
 def try_add(x, y):
-    return x + y
+    return BuiltinVariable(operator.add, x.graph, DanglingTracker())(x, y)
 
 
 class TestRollback(TestCaseBase):
@@ -26,8 +30,8 @@ class TestRollback(TestCaseBase):
         graph = FunctionGraph(frame)
         a = paddle.to_tensor(1.0)
         b = paddle.to_tensor(2.0)
-        a = VariableFactory().from_value(a, graph, DummyTracker)
-        b = VariableFactory().from_value(b, graph, DummyTracker)
+        a = VariableFactory().from_value(a, graph, LocalTracker("a"))
+        b = VariableFactory().from_value(b, graph, LocalTracker("b"))
         out = compute(a, b)
         original_length = len(graph.sir_ctx.TOS.statements)
         memo = graph.save_memo()
