ed03df3cc76dafee79da3940e6192185d1ea2441
Difer <707065510@qq.com>
2023-06-27 20:54:25 +0800

    [CRC] add docstring for some Variables (#206)

--- sot/opcode_translator/executor/variables/base.py
+++ sot/opcode_translator/executor/variables/base.py
@@ -79,17 +79,45 @@ def map_variables(map_func, variables):
 
 
 class VariableFactory:
+    """
+    A factory class for creating variables from arbitrary values.
+
+    This class provides a set of registration and factory methods for creating variables
+    of different types based on the type of the input value.
+
+    """
+
     registered_funcs: dict[str, list[str]] = {"default": []}
     mapping_str_func: dict[str, FromValueFunc] = {}
 
     @staticmethod
     def default_from_value(value, graph, tracker):
+        """
+        A default factory function that creates an ObjectVariable from the given value.
+
+        Args:
+            value: The input value.
+            graph: The FunctionGraph object that this variable is associated with.
+            tracker: The Tracker object that tracks the information of this variable.
+
+        Returns:
+            ObjectVariable: A new ObjectVariable representing the input value.
+        """
         from .basic import ObjectVariable
 
         return ObjectVariable(value, graph, tracker)
 
     @staticmethod
     def register_from_value(*, successor: str | None = None):
+        """
+        A decorator that registers a from_value function with the VariableFactory class.
+
+        Args:
+            successor (str | None): The name of the successor registration group, or None if this is the default group.
+
+        Returns:
+            Callable: A wrapper function that registers the decorated function with the VariableFactory class.
+        """
         registered_funcs = VariableFactory.registered_funcs
         mapping_str_func = VariableFactory.mapping_str_func
 
@@ -114,6 +142,22 @@ class VariableFactory:
         *,
         debug_name: str | None = None,
     ):
+        """
+        Create a new variable object from the given value.
+
+        This method searches through the registered from_value functions to find one
+        that can create a variable object from the given value. If no matching function
+        is found, the default_from_value function is used.
+
+        Args:
+            value (Any): The input value.
+            graph (FunctionGraph | None): The FunctionGraph object that this variable is associated with.
+            tracker (Tracker): The Tracker object that tracks the information of this variable.
+            debug_name (str | None): An optional debug name for the variable.
+
+        Returns:
+            VariableBase: A new variable object representing the input value.
+        """
         registered_funcs = VariableFactory.registered_funcs
 
         def _find_var(key: str = "default"):
@@ -138,6 +182,14 @@ class VariableBase:
     """
     VariableBase is a basic concept and each symbols in VM stack is regarded as
     an Variable Object in symblic tracing process.
+
+    It is designed to wrap the variables traced by tracker.
+
+    Args:
+        tracker(Tracker): The Tracker object that tracks the information of this variable.
+
+    Note:
+        It serves as an abstract class and should not be instantiated directly.
     """
 
     tracker: Tracker
@@ -150,10 +202,23 @@ class VariableBase:
 
     @property
     def main_info(self) -> dict[str, Any]:
+        """
+        Wrap the variable with relevant information
+
+        Returns:
+            main_info: Main information of the variable.
+        """
         return {}
 
     @property
     def debug_info(self) -> dict[str, Any]:
+        """
+        Wrap the debug-related information of the variable.
+
+        Returns:
+            debug_name: The name of variable.
+            id: The id of variable.
+        """
         return {
             "debug_name": self.debug_name,
             "id": self.id,
@@ -161,6 +226,12 @@ class VariableBase:
 
     @property
     def debug_name(self) -> str:
+        """
+        Generate a debug_name for each variable.
+
+        Returns:
+            _debug_name: the name of variable.
+        """
         if self._debug_name is None:
             inputs = self.tracker.inputs
             if isinstance(self.tracker, GetItemTracker):
@@ -189,6 +260,12 @@ class VariableBase:
         return hash(self.id)
 
     def make_stringify_guard(self) -> StringifyExpression:
+        """
+        Create a StringifyExpression object that represents a guard expression for this variable.
+
+        Returns:
+            StringifyExpression: An object that contains the guard expression and the free variables used in the expression.
+        """
         assert (
             self.tracker.is_traceable()
         ), "Cannot make guard from a non-traceable variable."
@@ -227,6 +304,12 @@ class VariableBase:
         raise NotImplementException()
 
     def flatten_items(self) -> list[VariableBase]:
+        """
+        Recursively flatten the items in this container variable to a list of Variable objects.
+
+        Returns:
+            list[VariableBase]: A list of Variable objects.
+        """
         from .container import ContainerVariable
 
         if not isinstance(self, ContainerVariable):
@@ -248,6 +331,12 @@ class VariableBase:
         )
 
     def flatten_traceable_inputs(self) -> list[VariableBase]:
+        """
+        Recursively flatten the items in this traceable_inputs to a list of Variable objects.
+
+        Returns:
+            list[Variable]: A list of Variable objects.
+        """
         if self.tracker.is_traceable():
             return [self]
 
@@ -260,6 +349,16 @@ class VariableBase:
         pass
 
     def getattr(self, name: str):
+        """
+        Get the value of an attribute with the given name from the underlying object of this variable.
+
+        Args:
+            name(str): The name of the attribute to retrieve.
+
+        Returns:
+            Variable object: A new variable representing the value of the requested attribute,
+                             or a MethodVariable object if the attribute is a method.
+        """
         if not hasattr(self.value, name):
             raise InnerError(
                 f"{self.__class__.__name__} {self} has no attribute {name}"
@@ -312,6 +411,16 @@ class VariableBase:
         return output
 
     def __call__(self, *args, **kwargs):
+        """
+        Call the object represented by this variable with the given arguments.
+
+        Args:
+            *args: Positional arguments to pass to the object's __call__ method.
+            **kwargs: Keyword arguments to pass to the object's __call__ method.
+
+        Returns:
+            VariableBase: A new variable representing the result of calling the object's __call__ method.
+        """
         from .callable import BuiltinVariable, UserDefinedFunctionVariable
 
         class_var = VariableFactory.from_value(
