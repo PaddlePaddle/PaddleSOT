5b7058c99efdd9740c76bc5af03ce6295be11e70
SigureMo <sigure.qaq@gmail.com>
2023-06-28 14:30:17 +0800

    add log_level to repro ci error

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -1,27 +1,28 @@
 # 遍历目录下的所有 Python 文件
-export PYTHONPATH=$PYTHONPATH:../
-export STRICT_MODE=1
-export LOG_LEVEL=3
+STRICT_MODE=1 LOG_LEVEL=3 python tests/test_side_effects.py TestDictSideEffect.test_dict_delitem
+# export PYTHONPATH=$PYTHONPATH:../
+# export STRICT_MODE=1
+# export LOG_LEVEL=3
 
-failed_tests=()
+# failed_tests=()
 
-for file in ./test_*.py; do
-    # 检查文件是否为 Python 文件
-    if [ -f "$file" ]; then
-        echo Running: PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
-        # 执行文件
-        python "$file"
-        if [ $? -ne 0 ]; then
-            echo "run $file failed"
-            failed_tests+=("$file")
-        fi
-    fi
-done
+# for file in ./test_*.py; do
+#     # 检查文件是否为 Python 文件
+#     if [ -f "$file" ]; then
+#         echo Running: PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
+#         # 执行文件
+#         python "$file"
+#         if [ $? -ne 0 ]; then
+#             echo "run $file failed"
+#             failed_tests+=("$file")
+#         fi
+#     fi
+# done
 
-if [ ${#failed_tests[@]} -ne 0 ]; then
-    echo "failed tests file:"
-    for failed_test in "${failed_tests[@]}"; do
-        echo "$failed_test"
-    done
-    exit 1
-fi
+# if [ ${#failed_tests[@]} -ne 0 ]; then
+#     echo "failed tests file:"
+#     for failed_test in "${failed_tests[@]}"; do
+#         echo "$failed_test"
+#     done
+#     exit 1
+# fi
