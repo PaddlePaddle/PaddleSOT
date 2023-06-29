7abaa6f4ab6813d412597e17756d0f894b502a44
xiongkun <xiongkun03@baidu.com>
2023-06-29 08:15:47 +0000

    fix

--- tests/run_all_paddle_ci.sh
+++ tests/run_all_paddle_ci.sh
@@ -1,5 +1,7 @@
 export STRICT_MODE=0
 
+failed_tests=()
+
 for file in ./Paddle/test/dygraph_to_static/*.py; do
     # 检查文件是否为 Python 文件
     if [ -f "$file" ]; then
@@ -9,8 +11,17 @@ for file in ./Paddle/test/dygraph_to_static/*.py; do
         python -u "$file"
         if [ $? -ne 0 ]; then
             echo "run $file failed"
+            failed_tests+=("$file")
         else
             echo "run $file success"
         fi
     fi
 done
+
+if [ ${#failed_tests[@]} -ne 0 ]; then
+    echo "failed tests file:"
+    for failed_test in "${failed_tests[@]}"; do
+        echo "$failed_test"
+    done
+    exit 1
+fi
