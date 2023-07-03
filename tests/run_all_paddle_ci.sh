36c3518efe8fe984420b19f9a4cfe1e1d1787ae3
xiongkun <xiongkun03@baidu.com>
2023-07-03 08:22:01 +0000

    fix renset and resnetv2

--- tests/run_all_paddle_ci.sh
+++ tests/run_all_paddle_ci.sh
@@ -10,8 +10,6 @@ disabled_tests=(
     ${PADDLE_TEST_BASE}/test_list.py # side effect
     ${PADDLE_TEST_BASE}/test_sentiment.py # disabled unitcase by paddle
     ${PADDLE_TEST_BASE}/test_reinforcement_learning.py # 'CartPoleEnv' object has no attribute 'seed'
-    ${PADDLE_TEST_BASE}/test_resnet_v2.py # segment error: oneDNN
-    ${PADDLE_TEST_BASE}/test_resnet.py # segment error: oneDNN
     # tmp = x
     # for i in range(x)
     #     tmp += Linear(x)
