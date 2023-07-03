6c20d40f4ba370050f13cee831583a578456507d
WangZhen <23097963+0x45f@users.noreply.github.com>
2023-07-03 21:06:06 +0800

    Fix dispatch getattr error (#233)

--- tests/test_builtin_dispatch.py
+++ tests/test_builtin_dispatch.py
@@ -22,5 +22,17 @@ class TestBuiltinDispatch(TestCaseBase):
         self.assert_results(dispatch_bool, paddle.to_tensor([1, 2, 3]))
 
 
+def run_getattr(x: paddle.Tensor):
+    attr = 'dtype'
+    out = getattr(x, attr)
+    return out
+
+
+class TestGetattr(TestCaseBase):
+    def test_getattr(self):
+        x = paddle.to_tensor(4)
+        self.assert_results(run_getattr, x)
+
+
 if __name__ == "__main__":
     unittest.main()
