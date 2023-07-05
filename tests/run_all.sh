7b6d68e25c32bf43510fab260bf1cce8c88df2cf
Zhan Rongrui <2742392377@qq.com>
2023-07-05 15:35:20 +0000

    test
    test

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -7,6 +7,7 @@ failed_tests=()
 for file in ./test_*.py; do
     # 检查文件是否为 Python 文件
     if [ -f "$file" ]; then
+        echo "::group::test $file"
         echo Running: PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
         # 执行文件
         python "$file"
@@ -14,16 +15,15 @@ for file in ./test_*.py; do
             echo "run $file failed"
             failed_tests+=("$file")
         fi
+        echo "::endgroup::"
     fi
 done
-echo "::group::My title"
-echo "Inside group"
-echo "::endgroup::"
 
 if [ ${#failed_tests[@]} -ne 0 ]; then
     echo "failed tests file:"
     for failed_test in "${failed_tests[@]}"; do
         echo "$failed_test"
+        echo "::error file=$failed_test,line=1,col=5,endColumn=7::Missing semicolon"
     done
     exit 1
 fi
