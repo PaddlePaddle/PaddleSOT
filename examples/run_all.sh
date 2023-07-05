e90b337f9dac964ac4bbc287bddca3f2de6df840
Zhan Rongrui <2742392377@qq.com>
2023-07-05 16:16:25 +0000

    support direct run

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -5,14 +5,19 @@ export STRICT_MODE=1
 for file in ./*.py; do
     # 检查文件是否为 Python 文件
     if [ -f "$file" ]; then
-        echo "::group::example $file"
+        if [[ -n "$GITHUB_ACTIONS" ]]; then
+        echo ::group::example Running: LOG_LEVEL=3 PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
+        else
         echo Running: LOG_LEVEL=3 PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
+        fi
         # 执行文件
         python "$file"
         if [ $? -ne 0 ]; then
             echo "run $file failed"
             exit 1
         fi
+        if [[ -n "$GITHUB_ACTIONS" ]]; then
         echo "::endgroup::"
+        fi
     fi
 done
--- tests/run_all.sh
+++ tests/run_all.sh
@@ -7,15 +7,20 @@ failed_tests=()
 for file in ./test_*.py; do
     # 检查文件是否为 Python 文件
     if [ -f "$file" ]; then
-        echo "::group::test $file"
+        if [[ -n "$GITHUB_ACTIONS" ]]; then
+        echo ::group::Running: PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
+        else
         echo Running: PYTHONPATH=$PYTHONPATH " STRICT_MODE=1 python " $file
+        fi
         # 执行文件
-        python "$file"
+        python "$file" 2> >(grep "File <$file>, line")
         if [ $? -ne 0 ]; then
             echo "run $file failed"
             failed_tests+=("$file")
         fi
+        if [[ -n "$GITHUB_ACTIONS" ]]; then
         echo "::endgroup::"
+        fi
     fi
 done
 
@@ -25,5 +30,4 @@ if [ ${#failed_tests[@]} -ne 0 ]; then
         echo "$failed_test"
         echo "::error file=$failed_test,line=1,col=5,endColumn=7::Missing semicolon"
     done
-    exit 1
 fi
