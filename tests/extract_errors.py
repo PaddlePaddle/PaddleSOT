d7b1e2dc1a730864fae0881a0308462c64124307
Zhan Rongrui <2742392377@qq.com>
2023-07-06 14:19:48 +0000

    fixbug

--- tests/extract_errors.py
+++ tests/extract_errors.py
@@ -6,11 +6,10 @@ error_msg = sys.stdin.read()
 pattern = r'File "?(.*?)"?, line (\d+),.*\n(.*?)\n(.*?)$'
 for match in re.finditer(pattern, error_msg, re.MULTILINE):
     file = match.group(1)
-    if not file.startswith("/"):
-        file = f"tests/{file}"
-    line = match.group(2)
-    error_info = match.group(4)
-    # error_info = match.group(3) + '\n' + match.group(4)
-    output = f"::error file={file},line={line}::{error_info}"
-    print(f"file={file},line={line}::{error_info}")
-    print(output)
+    if file.startswith("./"):
+        file = f"tests/{file[2:]}"
+        line = match.group(2)
+        error_info = match.group(4)
+        # error_info = match.group(3) + '\n' + match.group(4)
+        output = f"::error file={file},line={line}::{error_info}"
+        print(output)
