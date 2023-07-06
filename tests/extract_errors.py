2b9f2c433ca52da96bb1a5b9ba02506e8e4415af
Zhan Rongrui <2742392377@qq.com>
2023-07-06 13:47:49 +0000

    improve

--- tests/extract_errors.py
+++ tests/extract_errors.py
@@ -4,11 +4,12 @@ import sys
 error_msg = sys.stdin.read()
 
 pattern = r'File "?(.*?)"?, line (\d+),.*\n(.*?)\n(.*?)$'
-match = re.search(pattern, error_msg, re.MULTILINE)
-if match:
+for match in re.finditer(pattern, error_msg, re.MULTILINE):
     file = match.group(1)
+    if not file.startswith("/"):
+        file = f"tests/{file}"
     line = match.group(2)
     error_info = match.group(4)
     # error_info = match.group(3) + '\n' + match.group(4)
-    output = f"::error file=tests/{file},line={line}::{error_info}"
+    output = f"::error file={file},line={line}::{error_info}"
     print(output)
