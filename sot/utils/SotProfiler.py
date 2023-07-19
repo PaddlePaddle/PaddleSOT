19e364a6891a72597787e7301c82657e5d1ff4b4
feifei-111 <2364819892@qq.com>
2023-07-19 03:43:03 +0000

    update

---
+++ sot/utils/SotProfiler.py
@@ -0,0 +1,146 @@
+import os
+import time
+from contextlib import contextmanager
+from functools import wraps
+
+_Profilers = set()
+
+
+def _clear_profilers():
+    profilers = set(_Profilers)
+    for profiler in profilers:
+        profiler.disable()
+
+
+# atexit.register(_clear_profilers)
+
+
+class SotProfiler:
+    def __init__(self, name=None, outpath=None):
+        self.name = name
+        self.outpath = outpath
+        self.event_roots = []
+        self.event_stack = []
+
+    def __del__(self):
+        self.disable()
+        self.dump_report()
+
+    def dump_report(self):
+        def print_event(node, prefix):
+            print(prefix + str(node))
+            for sub_node in node.sub_events:
+                print_event(sub_node, prefix + "    ")
+
+        for root in self.event_roots:
+            # TODO
+            print_event(root, "")
+
+    def enable(self, tag=None):
+        if self not in _Profilers:
+            if tag is None:
+                tag = "Main"
+            _Profilers.add(self)
+            self.event_roots.append(EventNode(EventMeta(tag)))
+            self.event_roots[-1].hold.start()
+            self.event_stack = [self.event_roots[-1]]
+
+    def disable(self):
+        if self in _Profilers:
+            self.event_roots[-1].hold.end()
+            _Profilers.remove(self)
+
+    def push_event(self, event):
+        try:
+            node = self.event_stack[-1].push_event(event)
+        except:
+            breakpoint()
+        self.event_stack.append(node)
+
+    def pop_event(self, event):
+        self.event_stack.pop()
+
+
+@contextmanager
+def ProfileGuard(name=None, outpath=None):
+    profiler = SotProfiler(name, outpath)
+    try:
+        profiler.enable()
+        yield
+    finally:
+        profiler.disable()
+
+
+# the key infomations for events, there is only one EventMeta will be created when one Event triggered
+class EventMeta:
+    def __init__(self, name):
+        self.name = name
+        self.start_time = None
+        self.end_time = None
+        self.lasted = None
+
+    def start(self):
+        self.start_time = time.perf_counter()
+
+    def end(self):
+        if self.end_time is None:
+            self.end_time = time.perf_counter()
+            self.lasted = self.end_time - self.start_time
+
+    def __repr__(self):
+        return f"[Event: {self.name}](start: {self.start_time}, end: {self.end_time}, lasted: {self.lasted})"
+
+
+# EventNode is used for build tree struct of Events, every profiler holds EventNode as roots (if enable and disable called multi times)
+class EventNode:
+    def __init__(self, event: EventMeta):
+        self.hold = event
+        self.sub_events = []
+
+    def push_event(self, event):
+        self.sub_events.append(EventNode(event))
+        return self.sub_events[-1]
+
+    def __repr__(self):
+        return self.hold.__repr__()
+
+
+def event_start(event_name, event_level=0):
+    if not _Profilers or event_level < int(os.environ.get("EVENT_LEVEL", "0")):
+        return None
+    new_event = EventMeta(event_name)
+    for profile in _Profilers:
+        profile.push_event(new_event)
+    new_event.start()
+    return new_event
+
+
+def event_end(event):
+    if event is not None:
+        event.end()
+        for profile in _Profilers:
+            profile.pop_event(event)
+
+
+def event_decorator(event_name, event_level=0):
+    def event_wrapper(func):
+        @wraps(func)
+        def call_with_event(*args, **kwargs):
+            new_event = event_start(event_name, event_level)
+            try:
+                func(*args, **kwargs)
+            finally:
+                event_end(new_event)
+
+        return call_with_event
+
+    return event_wrapper
+
+
+@contextmanager
+def EventGuard(event_name, event_level=0):
+    try:
+        new_event = event_start(event_name, event_level)
+        yield
+    finally:
+        event_end(new_event)
