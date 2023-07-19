86bda99368f5f387c396e0ea2a30aed138958c71
Zhan Rongrui <2742392377@qq.com>
2023-07-19 14:44:55 +0000

    fix

--- tests/extract_errors.py
+++ tests/extract_errors.py
@@ -10,6 +10,7 @@ for match in re.finditer(pattern, runtime_error_msg, re.MULTILINE):
         file = f"tests/{file[2:]}"
         line = match.group(2)
         error_info = match.group(4)
-        # error_info = match.group(3) + '\n' + match.group(4)
-        output = f"::error file={file},line={line}::Error"
-        print(output)
+        if "AssertionError" not in error_info:
+            # error_info = match.group(3) + '\n' + match.group(4)
+            output = f"::error file={file},line={line}::Error"
+            print(output)
