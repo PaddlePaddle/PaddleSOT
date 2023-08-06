76957b6176cd428a81820db654c40bb9d6cc6b06
gouzi <530971494@qq.com>
2023-08-06 17:34:02 +0800

    support tensor unpack

--- tests/test_07_unpack.py
+++ tests/test_07_unpack.py
@@ -15,9 +15,15 @@ def foo(x: tuple[int, paddle.Tensor]):
     return z + 1
 
 
+def unpack_tensor(x: paddle.Tensor):
+    a, b = x
+    return (a, b)
+
+
 class TestExecutor(TestCaseBase):
     def test_simple(self):
         self.assert_results(foo, (1, paddle.to_tensor(2)))
+        self.assert_results(unpack_tensor, paddle.to_tensor([2, 3]))
 
 
 if __name__ == "__main__":
