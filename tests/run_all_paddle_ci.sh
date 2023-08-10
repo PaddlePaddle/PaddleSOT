c58ae2fd2fef6efe7ab6f9e45ddd82c02beaf869
SigureMo <sigure.qaq@gmail.com>
2023-08-10 10:38:46 +0800

    skip test_inplace_assign

--- tests/run_all_paddle_ci.sh
+++ tests/run_all_paddle_ci.sh
@@ -19,6 +19,7 @@ disabled_tests=(
     ${PADDLE_TEST_BASE}/test_ptb_lm.py # There is accuracy problem of the model in SOT
     ${PADDLE_TEST_BASE}/test_ptb_lm_v2.py # There is accuracy problem of the model in SOT
     ${PADDLE_TEST_BASE}/test_cycle_gan.py # This test has a precision problem when it reaches the maximum cache size
+    ${PADDLE_TEST_BASE}/test_inplace_assign.py # This test waiting for #301
 )
 
 for file in ${PADDLE_TEST_BASE}/*.py; do
