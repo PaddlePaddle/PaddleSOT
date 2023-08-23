03e323d8c35fca785b1525eb3e29573764c3d2c2
feifei-111 <2364819892@qq.com>
2023-08-23 16:45:30 +0800

    add _reconstruct for sequence_iterator (#335)

--- sot/opcode_translator/executor/variables/iter.py
+++ sot/opcode_translator/executor/variables/iter.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any
 
 from ....utils import InnerError
+from ..pycode_generator import PyCodeGen
 from ..tracker import ConstTracker, DummyTracker
 from .base import VariableBase
 from .basic import ConstantVariable, TensorVariable
@@ -33,6 +34,7 @@ class SequenceIterVariable(IterVariable):
     def __init__(self, obj, graph: FunctionGraph, tracker: Tracker):
         super().__init__(obj, graph, tracker)
         self.idx = 0
+        self.no_reconstruct = False
 
     def next(self):
         # TODO: self.hold should have a __len__ method
@@ -41,6 +43,7 @@ class SequenceIterVariable(IterVariable):
             self.idx += 1
             return val
         else:
+            self.no_reconstruct = True
             raise StopIteration()
 
     def to_list(self) -> list:
@@ -54,6 +57,46 @@ class SequenceIterVariable(IterVariable):
         return {
             "idx": self.idx,
         }
+
+    def _reconstruct(self, codegen: PyCodeGen):
+        """
+        The builtin function `next` is not supported, if we want to support that,
+        this function should be rewrite!!!!!!
+
+        NOTE:
+            SequenceIterVariable can only be created by GET_ITER opcode.
+            So SequenceIterVariable can be rebuild if the function input is specified.
+
+            SequenceIterVariable means this iter is created from list/tuple/range,
+            we will not change the behavour after rebuild.
+
+            1. Why _reconstruct might not OK?
+                the iterator can be inplacely changed by will next or for
+                however, we can only reconstruct a iterator with idx==0.
+
+            2. Why _reconstruct is OK?
+                currently, next is not supported (lead to breakgraph)
+                so the idx of iter can only change in for loop
+
+                there are only 3 cases:
+                ---------------------
+                    codes1
+                    for iter
+                        codes2
+                    codes3
+                ---------------------
+                For codes1, _reconstruct is always OK (idx is always 0 for next is not supported)
+                For codes2, though self.idx is not reset propertily, the whole loop will rerun
+                    in dynamic mode, so rebuild a iterator with idx=0 is OK
+                For codes3, rebuild a iterator with idx=0 is bad, we will call super()._reconstruct
+                    instead
+
+        """
+        if self.no_reconstruct:
+            super()._reconstruct(codegen)
+        else:
+            self.hold._reconstruct(codegen)
+            codegen.gen_get_iter()
 
 
 class EnumerateVariable(IterVariable):
