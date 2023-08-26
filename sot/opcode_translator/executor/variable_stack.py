5d0ade79d566a76fee2a23016ca0988f3d8bb381
Zhan Rongrui <46243324+zrr1999@users.noreply.github.com>
2023-08-26 15:31:09 +0800

    [refactor] use VariableStack instead of list (#343)
    Co-authored-by: Nyakku Shigure <sigure.qaq@gmail.com>

---
+++ sot/opcode_translator/executor/variable_stack.py
@@ -0,0 +1,182 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Callable, Generic, TypeVar, overload
+
+if TYPE_CHECKING:
+    ValidateValueFunc = Callable[[Any], None]
+
+
+StackDataT = TypeVar("StackDataT")
+
+
+class VariableStack(Generic[StackDataT]):
+    """
+    A stack class for storing variables.
+
+    Examples:
+        >>> var1, var2, var3, var4 = range(1, 5)
+        >>> stack = VariableStack()
+        >>> stack.push(var1)
+        >>> stack.push(var3)
+        >>> stack.insert(1, var2)
+        >>> stack
+        [1, 2, 3]
+        >>> stack.pop()
+        3
+        >>> stack.pop_n(2)
+        [1, 2]
+        >>> stack.push(var1)
+        >>> stack.push(var2)
+        >>> stack.push(var3)
+        >>> stack
+        [1, 2, 3]
+        >>> stack.top
+        3
+        >>> stack.peek[1]
+        3
+        >>> stack.peek[:1]
+        [3]
+        >>> stack.peek[:2]
+        [2, 3]
+        >>> stack.peek[1] = var4
+        >>> stack
+        [1, 2, 4]
+
+    """
+
+    class VariablePeeker:
+        @overload
+        def __getitem__(self, index: int) -> StackDataT:
+            ...
+
+        @overload
+        def __getitem__(self, index: slice) -> list[StackDataT]:
+            ...
+
+        @overload
+        def __call__(self, index: int = 1) -> StackDataT:
+            ...
+
+        @overload
+        def __call__(self, index: slice) -> list[StackDataT]:
+            ...
+
+        def __init__(
+            self, data: list[StackDataT], validate_value: ValidateValueFunc
+        ):
+            self._data = data
+            self.validate_value = validate_value
+
+        def __getitem__(
+            self, index: int | slice
+        ) -> StackDataT | list[StackDataT]:
+            if isinstance(index, int):
+                assert index >= 0
+                return self._data[-index]
+            if isinstance(index, slice):
+                assert index.start is None
+                assert index.step is None
+                assert index.stop > 0
+                return self._data[-index.stop :]
+            raise NotImplementedError(f"index type {type(index)} not supported")
+
+        def __setitem__(self, index: int, value: Any):
+            assert isinstance(
+                index, int
+            ), f"index type {type(index)} not supported"
+            self.validate_value(value)
+            self._data[-index] = value
+
+        def __call__(
+            self, index: int | slice = 1
+        ) -> StackDataT | list[StackDataT]:
+            return self[index]
+
+    def __init__(
+        self,
+        data: list[StackDataT] | None = None,
+        *,
+        validator: ValidateValueFunc | None = None,
+    ):
+        self.validate_value = (
+            (lambda _: None) if validator is None else validator
+        )
+        self._data = data or []
+        self._peeker = VariableStack.VariablePeeker(
+            self._data, self.validate_value
+        )
+
+    def copy(self):
+        return VariableStack(self._data.copy(), validator=self.validate_value)
+
+    def push(self, val: StackDataT):
+        """
+        Pushes a variable onto the stack.
+
+        Args:
+            val: The variable to be pushed.
+
+        """
+        self.validate_value(val)
+        self._data.append(val)
+
+    def insert(self, index: int, val: StackDataT):
+        """
+        Inserts a variable onto the stack.
+
+        Args:
+            index: The index at which the variable is to be inserted, the top of the stack is at index 0.
+            val: The variable to be inserted.
+
+        """
+        assert index >= 0
+        self.validate_value(val)
+        self._data.insert(len(self) - index, val)
+
+    def pop(self) -> StackDataT:
+        """
+        Pops the top value from the stack.
+
+        Returns:
+            The popped value.
+
+        """
+        return self._data.pop()
+
+    def pop_n(self, n: int) -> list[StackDataT]:
+        """
+        Pops the top n values from the stack.
+
+        Args:
+            n: The number of values to pop.
+
+        Returns:
+            A list of the popped values.
+
+        """
+        assert (
+            len(self) >= n >= 0
+        ), f"n should be in [0, {len(self)}], but get {n}"
+        if n == 0:
+            return []
+        retval = self._data[-n:]
+        self._data[-n:] = []
+        return retval
+
+    @property
+    def peek(self):
+        return self._peeker
+
+    @property
+    def top(self):
+        return self.peek[1]
+
+    @top.setter
+    def top(self, value):
+        self.peek[1] = value
+
+    def __len__(self):
+        return len(self._data)
+
+    def __repr__(self) -> str:
+        return str(self._data)
---
+++ tests/test_stack.py
@@ -0,0 +1,40 @@
+import unittest
+
+from sot.opcode_translator.executor.variable_stack import VariableStack
+
+
+class TestVariableStack(unittest.TestCase):
+    def test_basic(self):
+        stack = VariableStack([1, 2, 3])
+        self.assertEqual(str(stack), "[1, 2, 3]")
+        self.assertEqual(len(stack), 3)
+        self.assertEqual(str(stack.copy()), str(stack))
+
+    def test_peek(self):
+        stack = VariableStack([1, 2, 3])
+        self.assertEqual(stack.peek(), 3)
+        self.assertEqual(stack.top, 3)
+        self.assertEqual(stack.peek(1), 3)
+        stack.peek[1] = 4
+        stack.peek[2] = 3
+        self.assertEqual(stack.peek[1], 4)
+        self.assertEqual(stack.peek[:1], [4])
+        self.assertEqual(stack.peek[:2], [3, 4])
+        stack.top = 5
+        self.assertEqual(stack.peek[:2], [3, 5])
+
+    def test_push_pop(self):
+        stack = VariableStack()
+        stack.push(1)
+        stack.push(2)
+        self.assertEqual(stack.pop(), 2)
+        self.assertEqual(stack.pop(), 1)
+
+    def test_pop_n(self):
+        stack = VariableStack([1, 2, 3, 4])
+        self.assertEqual(stack.pop_n(2), [3, 4])
+        self.assertEqual(stack.pop_n(2), [1, 2])
+
+
+if __name__ == "__main__":
+    unittest.main()
