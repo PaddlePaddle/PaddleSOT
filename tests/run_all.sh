9810b18cd22e835f7af78920a1b714984031e2f2
gouzi <530971494@qq.com>
2023-08-30 17:38:43 +0800

    add test

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -16,7 +16,7 @@ py311_skiped_tests=(
     ./test_11_jumps.py
     ./test_12_for_loop.py
     ./test_13_make_function.py
-    ./test_14_operators.py
+    # ./test_14_operators.py        There are some case need to be fixed
     ./test_15_slice.py
     ./test_17_paddle_layer.py
     ./test_18_tensor_method.py
