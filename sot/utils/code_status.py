adbd5d517dc7a396562a87cb7fa5ce28f93a9918
feifei-111 <2364819892@qq.com>
2023-09-13 03:48:27 +0000

    update

--- sot/utils/code_status.py
+++ sot/utils/code_status.py
@@ -22,6 +22,9 @@ class CodeInfo:
 @Singleton
 class CodeStatus:
     def __init__(self):
+        """
+        code can not be the key, for we might add unhashable object into co_consts
+        """
         self.code_map = {}
         self.skip_count = 0
 
@@ -29,11 +32,11 @@ class CodeStatus:
         self.code_map.clear()
 
     def check_code(self, code):
-        if code not in self.code_map:
+        if id(code) not in self.code_map:
             info = CodeInfo(code)
-            self.code_map[code] = info
+            self.code_map[id(code)] = info
         else:
-            info = self.code_map[code]
+            info = self.code_map[id(code)]
 
         if info.state == CodeState.WITHOUT_GRAPH:
             return True
@@ -42,7 +45,7 @@ class CodeStatus:
         return False
 
     def visit(self, code):
-        info = self.code_map[code]
+        info = self.code_map[id(code)]
         info.counter += 1
         if info.state == CodeState.UNKNOW and info.counter > 10:
             self.skip_count += 1
@@ -53,6 +56,6 @@ class CodeStatus:
         while frame.f_back is not None:
             frame = frame.f_back
             code = frame.f_code
-            if code in self.code_map:
-                info = self.code_map[code]
+            if id(code) in self.code_map:
+                info = self.code_map[id(code)]
                 info.state = CodeState.WITH_GRAPH
