4371abea8f431597276d1db5b597f92f557c2c97
feifei-111 <2364819892@qq.com>
2023-10-08 17:47:37 +0800

    update skip files (#408)

--- sot/opcode_translator/executor/guard.py
+++ sot/opcode_translator/executor/guard.py
@@ -33,11 +33,11 @@ class StringifyExpression:
     Used to store string based expressions for generating Guard.
     """
 
-    def __init__(self, str_expr, format_args, free_vars):
-        expr = str_expr.format(*[arg.expr for arg in format_args])
+    def __init__(self, str_expr, sub_exprs, free_vars):
+        expr = str_expr.format(*[arg.expr for arg in sub_exprs])
         self.expr = current_tmp_name_records().add_tmp_var(expr)
         self.debug_expr = str_expr.format(
-            *[arg.debug_expr for arg in format_args]
+            *[arg.debug_expr for arg in sub_exprs]
         )
         self.free_vars = free_vars
 
@@ -92,7 +92,7 @@ def make_guard(stringify_guards: list[StringifyExpression]) -> Guard:
                 lambda_string += str_expr.debug_expr + " and "
                 free_vars = union_free_vars(free_vars, str_expr.free_vars)
 
-            func_string += f"    return {func_result[:-5]}\n"
+            func_string += f"    return {func_result[:-5]}"
 
             return func_string, free_vars, lambda_string[:-5]
 
--- sot/opcode_translator/transform.py
+++ sot/opcode_translator/transform.py
@@ -47,44 +47,47 @@ def eval_frame_callback(frame, **kwargs) -> CustomCode:
             return CustomCode(None, True)
 
         if need_skip(frame):
-            return CustomCode(None, False)
-
-        log(
-            2,
-            "[eval_frame_callback] start to translate: "
-            + str(frame.f_code)
-            + "\n",
-        )
-        log_do(4, partial(print_locals, frame))
-
-        log(3, f"[transform] OriginCode: {frame.f_code.co_name}\n")
-        log_do(3, lambda: dis.dis(frame.f_code))
-
-        custom_code = InstructionTranslatorCache()(frame, **kwargs)
-
-        if custom_code.code is None:
-            log(
-                3,
-                "[transform] NewCode (same as origin code): "
-                + frame.f_code.co_name
-                + "\n",
-            )
-            used_code = frame.f_code
+            log(3, f"[eval_frame_callback] skip {frame.f_code}\n")
+            custom_code = CustomCode(None, False)
+            new_code = frame.f_code
         else:
             log(
-                3,
-                "[transform] NewCode: " + custom_code.code.co_name + "\n",
+                2,
+                "[eval_frame_callback] start to translate: "
+                + str(frame.f_code)
+                + "\n",
             )
-            log_do(3, lambda: dis.dis(custom_code.code))
-            used_code = custom_code.code
+            log_do(4, partial(print_locals, frame))
+
+            log(3, f"[transform] OriginCode: {frame.f_code.co_name}\n")
+            log_do(3, lambda: dis.dis(frame.f_code))
+
+            custom_code = InstructionTranslatorCache()(frame, **kwargs)
+
+            if custom_code.code is None:
+                log(
+                    3,
+                    "[transform] NewCode (same as origin code): "
+                    + frame.f_code.co_name
+                    + "\n",
+                )
+                new_code = frame.f_code
+            else:
+                log(
+                    3,
+                    "[transform] NewCode: " + custom_code.code.co_name + "\n",
+                )
+                log_do(3, lambda: dis.dis(custom_code.code))
+                new_code = custom_code.code
 
         # just check those codes which need open eval_frame
-        if custom_code.disable_eval_frame is False and CodeStatus().check_code(
-            used_code
+        if (
+            custom_code.disable_eval_frame is False
+            and CodeStatus().is_code_without_graph(new_code)
         ):
             log(
                 3,
-                "[transform] Code has found no graph, block it.",
+                "[eval_frame_callback] Code has no graph, block it.",
             )
             return CustomCode(None, True)
 
